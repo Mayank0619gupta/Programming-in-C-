959436d4b2d040a9578e1319e40992bd9dd4e1ae
Mayank Gupta <150025164+Mayank0619gupta@users.noreply.github.com>
2025-08-14 20:02:20 +0530

    Fibonacci Series.cpp

---
+++ Fibonacci Series.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,x,y,z;
+    cout<<"Enter the number to check: "<<endl;
+    cin>>n;
+    x=0;
+    y=1;
+    z=0;
+    while(z<=n){
+        cout<<" "<<z;
+        x=y;
+        y=z;
+        z=x+y;
+    }
+    return 0;
+}
